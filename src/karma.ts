76c9992720bf574d87766a67b08df0765af08117
qxxst <qxxst@proton.me>
2024-03-15 06:50:22 -0500

    Update karma.ts

--- src/karma.ts
+++ src/karma.ts
@@ -1,6 +1,6 @@
 // karma.js by qxxst
 /** @param {NS} ns */
-export async function main(ns) {
+export async function main(ns: any) {
     const karma = ns.heart.break();
     ns.tprint(`Current karma: ${karma}`);
     ns.tprint('Start with Mug until your stats get up a bit, and then farm Homicide. Murder is the best karma/second act you can do.');
