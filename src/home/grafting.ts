d0752e3c73c4affb9e4d67cb163a3fa43b2c849a
qxxst <qxxst@proton.me>
2024-03-16 01:27:21 -0500

    cleanup and add comments

--- src/home/grafting.ts
+++ src/home/grafting.ts
@@ -2,7 +2,8 @@
 /** @param {NS} ns */
 export async function main(ns: any) {
     // Settings
-    const focus: boolean = true;
+    const focus: boolean = true; // Whether or not you want the grafting task to be focused on. This makes it go faster but prevents you from doing other things while it's working.
+    const filter: string | null = "hack"; // The type of augs we specificaly want to graft. Set to null to graft everything.
 
     let currentCity: string = ns.getPlayer().location;
     if (currentCity !== "New Tokyo") {
@@ -13,8 +14,8 @@ export async function main(ns: any) {
     let graftableAugs: string[];
     let augsRemaining: number;
     graftableAugs = ns.grafting.getGraftableAugmentations();
-    
     augsRemaining = graftableAugs.length;
+
     function check(): void {
         graftableAugs = ns.grafting.getGraftableAugmentations();
         augsRemaining = graftableAugs.length;
