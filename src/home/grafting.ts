3a5aef8a4f6e790fc195e11d100a3805d52e08a8
qxxst <qxxst@proton.me>
2024-03-15 22:12:23 -0500

    Wait for the aug to finish

--- src/home/grafting.ts
+++ src/home/grafting.ts
@@ -1,6 +1,10 @@
 // grafting.js by qxxst
 /** @param {NS} ns */
 export async function main(ns: any) {
+    // Settings
+    const focus: boolean = true;
+    const cooldown: number = 1000;
+
     let currentCity: string = ns.getPlayer().location;
     if (currentCity !== "New Tokyo") {
         ns.singularity.travelToCity("New Tokyo");
@@ -10,8 +14,6 @@ export async function main(ns: any) {
     let graftableAugs: string[];
     let augsRemaining: number;
     graftableAugs = ns.grafting.getGraftableAugmentations();
-
-    const focus: boolean = true;
     
     augsRemaining = graftableAugs.length;
     function check() {
@@ -20,7 +22,12 @@ export async function main(ns: any) {
     }
 
     for (let aug of graftableAugs) {
+        let time: number = ns.grafting.getAugmentationGraftTime(aug);
+
         ns.grafting.graftAugmentation(aug, focus);
+
+        await ns.sleep(time + cooldown);
+        
         check();
     }
 }
