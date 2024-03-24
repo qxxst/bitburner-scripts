f729663d0c61b98c1e4cbf9938fad3bbd4ee850e
qxxst <qxxst@proton.me>
2024-03-24 02:01:32 -0500

    turn off this stupid shit

--- dist/home/template.js
+++ dist/home/template.js
@@ -3,4 +3,3 @@
 export async function main(ns) {
     // code
 }
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidGVtcGxhdGUuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvaG9tZS90ZW1wbGF0ZS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSxpQkFBaUI7QUFDakIscUJBQXFCO0FBQ3JCLE1BQU0sQ0FBQyxLQUFLLFVBQVUsSUFBSSxDQUFDLEVBQU87SUFDakMsT0FBTztBQUNSLENBQUMifQ==
