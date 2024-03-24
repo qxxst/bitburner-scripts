00cb536e852ee17c390fe288c86d5761382461f9
qxxst <qxxst@proton.me>
2024-03-24 14:56:35 -0500

    yeet

--- src/home/nuke.ts
+++ src/home/nuke.ts
@@ -1,18 +1,7 @@
 // nuke.js by qxxst
-import {sourceFileOwned, currentBitnode} from 'lib/util';
-import {getEnv} from 'env';
-
-let Scanner: any;
-if (getEnv().imQxxst) {
-    import("lib/scan").then((module) => {
-        Scanner = module.Scanner;
-    })
-}
-
 /** @param {NS} ns */
 export async function main(ns: any) {
 	const target: string = ns.args[0];
-    const factionServers: readonly string[] = ["CSEC", "avmnite-02h", "I.I.I.I", "run4theh111z"];
 
     if (ns.fileExists("BruteSSH.exe", "home")) {
         ns.brutessh(target);
@@ -31,35 +20,4 @@ export async function main(ns: any) {
     }
     
     ns.nuke(target);
-    if (sourceFileOwned(ns, 4, 1) || currentBitnode(ns) == 4) {
-        if (getEnv().imQxxst) {
-            let route_list: any = Scanner.route(ns, target, true);
-            try {
-                if (route_list) {
-                    let first_stop: any = route_list.shift(); // pop home off
-                    if (first_stop && first_stop != "home") {
-                        route_list.unshift(first_stop);
-                    }
-                    for (let link of route_list) {
-                        ns.singularity.connect(link);
-                    }
-                    await ns.singularity.installBackdoor();
-                    await ns.sleep(1000);
-                    ns.singularity.connect("home");
-                }
-            }
-            catch {
-                await ns.sleep(1000);
-            }
-        }
-        
-        if (factionServers.includes(target)) {
-            await ns.sleep(1000);
-
-            await ns.singularity.joinFaction("CyberSec");
-            await ns.singularity.joinFaction("NiteSec");
-            await ns.singularity.joinFaction("The Black Hand");
-            await ns.singularity.joinFaction("BitRunners");
-        }
-    }
 }
