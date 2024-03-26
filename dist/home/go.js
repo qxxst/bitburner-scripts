cde5ee068390850ef7726a539e31b26749a74b0b
qxxst <qxxst@proton.me>
2024-03-26 12:06:24 -0500

    check if go.cheat property is allowed

--- dist/home/go.js
+++ dist/home/go.js
@@ -1,5 +1,10 @@
 // go.js by qxxst
+import { sourceFileOwned } from 'lib/util.js';
 /** @param {NS} ns */
 export async function main(ns) {
     const desiredOpponent = "Illuminati";
+    let canCheat = false;
+    if (sourceFileOwned(ns, 14, 2)) {
+        canCheat = true;
+    }
 }
--- src/home/go.ts
+++ src/home/go.ts
@@ -1,5 +1,11 @@
 // go.js by qxxst
+import { sourceFileOwned } from 'lib/util.js';
 /** @param {NS} ns */
 export async function main(ns: any) {
 	const desiredOpponent: string = "Illuminati";
+
+    let canCheat: boolean = false;
+    if (sourceFileOwned(ns, 14, 2)) {
+        canCheat = true;
+    }
 }
